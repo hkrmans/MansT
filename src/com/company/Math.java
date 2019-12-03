5f7d28a5e5a77ad36889fd59e2b208b20882e5a1
hkrmans <mans.thomasson@gmail.com>
2019-12-03 11:14:03 +0100

    Changed the add method in math class

--- src/com/company/Math.java
+++ src/com/company/Math.java
@@ -2,8 +2,8 @@ package com.company;
 
 public class Math {
 
-    public int add(int first, int second) {
-        return first + second;
+    public int add(int first, int second, int forth) {
+        return first + second + forth;
     }
 
     public int sub(int first, int second) {
