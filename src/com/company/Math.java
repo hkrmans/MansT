ccc1e3ff025ae3306d472dc884385a825b2c108a
Valter Sjöstrand <Valter.sjo@gmail.com>
2019-12-03 11:11:47 +0100

    Changed add method

--- src/com/company/Math.java
+++ src/com/company/Math.java
@@ -2,8 +2,8 @@ package com.company;
 
 public class Math {
 
-    public int add(int first, int second) {
-        return first + second;
+    public int add(int first, int second, int third) {
+        return first + second + third;
     }
 
     public int sub(int first, int second) {
